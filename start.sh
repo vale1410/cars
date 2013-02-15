0d4b9dc04fe1c2508ca2ff08f05517d5d776b1f7
vale <vale1410@gmail.com>
2013-02-15 23:04:21 +1100

    start changed

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@
 source prll.sh
 
 data=${1-data/test}
-output=${2-output/model10_1800}
-time=${3-1800}
+output=${2-output/model9_1800}
+time=${3-1500}
 
 mkdir -p $output
 
@@ -23,13 +23,13 @@ all+=data/set4
 
 echo $all
 
-for x in {1..2}
+for x in 1
 do
     r=$[${RANDOM}%100000]
     for data in $all
     do
         echo $data
-        for model in 10
+        for model in 9
         do
             for conf in 4
             do
