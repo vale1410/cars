2a2c4a67fc12b8f3ba5b294b533a099be05cc7d0
vale <vale1410@gmail.com>
2013-03-01 11:12:11 +1100

    logs added

--- sat/model10/hard/start.sh
+++ sat/model10/hard/start.sh
@@ -2,19 +2,19 @@
 
 timeout=100000
 
-timeout $timeout lingeling pb_200_02.cnf --verbose | grep '^[^v]' >  pb_200_02.log &
-timeout $timeout lingeling pb_200_06.cnf --verbose | grep '^[^v]' >  pb_200_06.log &
-timeout $timeout lingeling pb_200_08.cnf --verbose | grep '^[^v]' >  pb_200_08.log &
-timeout $timeout lingeling pb_300_02.cnf --verbose | grep '^[^v]' >  pb_300_02.log &
-timeout $timeout lingeling pb_300_06.cnf --verbose | grep '^[^v]' >  pb_300_06.log &
-
-sleep $timeout
-
-timeout $timeout lingeling pb_300_09.cnf --verbose | grep '^[^v]' >  pb_300_09.log &
-timeout $timeout lingeling pb_400_01.cnf --verbose | grep '^[^v]' >  pb_400_01.log &
-timeout $timeout lingeling pb_400_02.cnf --verbose | grep '^[^v]' >  pb_400_02.log &
-timeout $timeout lingeling pb_400_07.cnf --verbose | grep '^[^v]' >  pb_400_07.log &
-timeout $timeout lingeling pb_400_08.cnf --verbose | grep '^[^v]' >  pb_400_08.log &
+clasp pb_200_02.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_200_02_clasp.log &
+clasp pb_200_06.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_200_06_clasp.log &
+clasp pb_200_08.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_200_08_clasp.log &
+clasp pb_300_02.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_300_02_clasp.log &
+clasp pb_300_06.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_300_06_clasp.log &
+                                                            
+sleep $timeout                                              
+                                                            
+clasp pb_300_09.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_300_09_clasp.log &
+clasp pb_400_01.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_400_01_clasp.log &
+clasp pb_400_02.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_400_02_clasp.log &
+clasp pb_400_07.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_400_07_clasp.log &
+clasp pb_400_08.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_400_08_clasp.log &
 
 
 
