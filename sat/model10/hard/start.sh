7c825044dc8a6f1813381eb2ba0f57986a97eddb
vale <vale1410@gmail.com>
2013-03-04 23:29:58 +1100

    crypto output added

--- sat/model10/hard/start.sh
+++ sat/model10/hard/start.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
 timeout=100000
+solver=5
 
-clasp pb_200_02.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_200_02_clasp.log &
-clasp pb_200_06.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_200_06_clasp.log &
-clasp pb_200_08.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_200_08_clasp.log &
-clasp pb_300_02.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_300_02_clasp.log &
-clasp pb_300_06.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_300_06_clasp.log &
-                                                            
+../../sat.sh pb_200_02.cnf $solver $timeout >  pb_200_02_crypto.log &
+../../sat.sh pb_200_06.cnf $solver $timeout >  pb_200_06_crypto.log &
+../../sat.sh pb_200_08.cnf $solver $timeout >  pb_200_08_crypto.log &
+../../sat.sh pb_300_02.cnf $solver $timeout >  pb_300_02_crypto.log &
+../../sat.sh pb_300_06.cnf $solver $timeout >  pb_300_06_crypto.log &
+
 sleep $timeout                                              
-                                                            
-clasp pb_300_09.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_300_09_clasp.log &
-clasp pb_400_01.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_400_01_clasp.log &
-clasp pb_400_02.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_400_02_clasp.log &
-clasp pb_400_07.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_400_07_clasp.log &
-clasp pb_400_08.cnf --time-limit=$timeout --configuration=crafty --stats >  pb_400_08_clasp.log &
 
-
-
-
-
-
-
-
-
-
-
-
-
+../../sat.sh pb_300_09.cnf $solver $timeout >  pb_300_09_crypto.log &
+../../sat.sh pb_400_01.cnf $solver $timeout >  pb_400_01_crypto.log &
+../../sat.sh pb_400_02.cnf $solver $timeout >  pb_400_02_crypto.log &
+../../sat.sh pb_400_07.cnf $solver $timeout >  pb_400_07_crypto.log &
+../../sat.sh pb_400_08.cnf $solver $timeout >  pb_400_08_crypto.log &
--- sat/sat.sh
+++ sat/sat.sh
@@ -11,7 +11,7 @@ timeout=${3-60}
 
 case $solver in
     1) timeout $timeout minisat $instance  ;;
-    2) timeout $timeout cryptominisat $instance | grep '^[^v]'   ;;
+    2) timeout $timeout cryptominisat $instance  ;;
     3) timeout $timeout lingeling --verbose $instance | grep '^[^v]'  ;;
     4) clasp --time-limit=$timeout -q --stats --configuration=trendy $instance  ;;
     5) timeout $timeout glucose_static $instance | grep '^[^v]' ;;
