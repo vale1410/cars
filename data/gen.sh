558fc46fd4bbeba4190ea2434b896b7274ecc37c
vale <vale1410@gmail.com>
2013-04-11 12:39:08 +1000

    lower bounds added

--- data/gen.sh
+++ data/gen.sh
@@ -5,6 +5,8 @@ do
     a=("${(s/ /)line}")
     for x in {00..$a[2]}
     do
-        echo gen -file $a[1] -e3 -sym -add $x to $(basename $a[1] .txt)_lb_$x.cnf
+        typeset -i v=$x
+        echo $a[1] $x
+        ../gen/gen1 -file $a[1] -e3 -re1 -re2 -add $v > cnf/$(basename $a[1] .txt)_lb_$x.cnf
     done
 done
