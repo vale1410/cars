d1f468e4fe0ea7c4665c752ebca53cc3d780cace
vale <vale1410@gmail.com>
2013-04-23 11:14:07 +1000

    stats added

--- data/gen.sh
+++ data/gen.sh
@@ -2,11 +2,16 @@
 
 while read line
 do
-    a=("${(s/ /)line}")
-    for x in {00..$a[2]}
-    do
-        typeset -i v=$x
-        echo $a[1] $x
-        ../gen/gen1 -file $a[1] -cnt -sym -re1 -re2 -id7 -id8 -id9 -add $v > cnf/$(basename $a[1] .txt)_lb_$x.cnf
+    for e in {e1,e2,e3,e4,e5}
+    do 
+        mkdir -p $e
+        mkdir -p $e/cnf
+        a=("${(s/ /)line}")
+        for x in {00..$a[2]}
+        do
+            typeset -i v=$x
+            echo $a[1] $x
+            ../gen/gen_sat -file $a[1] -$e -sym -add $v > $e/cnf/$(basename $a[1] .txt)_lb_$x.cnf
+        done
     done
 done
